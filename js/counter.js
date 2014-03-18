33a919edfbb3b058c56255497f85c74aa5ac3638
ank13 <avi.kaufman@gmail.com>
2014-03-18 19:42:53 -0400

    styling

--- js/counter.js
+++ js/counter.js
@@ -18,9 +18,4 @@ function updateCounts(){
  var charactersOnly = $('#user-text').val().replace(/ /g,'').length
  $('#char-only').html(charactersOnly)
 
- var tweetLeft = 140 - totalCharacters
- $('#tweet-left').html(tweetLeft)
-
- var smsLeft = 160 - totalCharacters
- $('#sms-left').html(smsLeft)
 }
