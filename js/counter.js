00700750f1f0cc77fd987a088bfbcb75d38e978b
ank13 <avi.kaufman@gmail.com>
2014-03-18 13:47:22 -0400

    adds styling and counters

--- js/counter.js
+++ js/counter.js
@@ -1,13 +1,29 @@
 $(document).ready(function(){
 
-  $('#user-text').on('keyup', function(){
+  $('#user-text').on('keyup keydown', function(){
+    updateCounts()
+  })
 
-    var total_characters = $(this).val().length
-    $('#char-count').html(total_characters)
-
-    var characters_only = $(this).val().replace(/ /g,'').length
-    $('#char-only').html(characters_only)
-
+  $('#clear-field').on('click', function(event){
+    event.preventDefault()
+    $('#user-text').val('')
+    updateCounts()
   })
 
 });
+
+
+function updateCounts(){
+ 
+ var totalCharacters = $('#user-text').val().length
+ $('#char-count').html(totalCharacters)
+
+ var charactersOnly = $('#user-text').val().replace(/ /g,'').length
+ $('#char-only').html(charactersOnly)
+
+ var tweetLeft = 140 - totalCharacters
+ $('#tweet-left').html(tweetLeft)
+
+ var smsLeft = 160 - totalCharacters
+ $('#sms-left').html(smsLeft)
+}
